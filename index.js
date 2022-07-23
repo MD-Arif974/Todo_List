78ec86812a300340754d67100e9e7593b31d45f7
MD Arif <arifreza974@gmail.com>
2022-07-23 09:18:25 +0530

    routes and controllers created

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const port = 8000;
 
 const app = express();
 
+// set our ejs and views and routes
+app.use('/',require('./routes'));
+app.set('view engine','ejs');
+app.set('views','./views');
+
+
+
+// app is listening on this port
 app.listen(port,function(err){
     if(err){
         console.log("Error while running server",err);
---
+++ routes/todo.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const homeControllers = require('../controllers/home_controllers');
+console.log("Router is loaded!");
+
+router.get('/',homeControllers.home);
+
+module.exports = router;
