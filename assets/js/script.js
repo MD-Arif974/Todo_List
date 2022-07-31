3d2bd0c95fb1cb75ae92a46a44e9c2344fa1f586
MD Arif <arifreza974@gmail.com>
2022-07-31 10:18:14 +0530

    checkbox function and deletebtn function working fine!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,2 @@
 
-let deleteBtn = document.querySelector(".deleteTask");
-let body = document.querySelector("body");
-
-
-
-function deleteAllTask() {
-    console.log("ander sab thik hai bhai");
-    body.style.backgroundColor = "red";
-    
-}
-deleteBtn.addEventListener("click",deleteAllTask);
 
